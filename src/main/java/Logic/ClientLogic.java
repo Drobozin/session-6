61ca49019867b3eecce9ed5690cde451296f6f3b
Andrew Drobozin <crash1601@gmail.com>
2018-04-27 18:26:41 +0300

    04_27

---
+++ src/main/java/Logic/ClientLogic.java
@@ -0,0 +1,95 @@
+package Logic;
+
+import problem.Client;
+import problem.Main;
+import problem2.JSON.JSONFormatterImpl;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ClientLogic {
+    private static final String usersPath = "src/main/java/problem/users";
+
+    public static String createClient(String request){
+        Map<String,String> clientParams = new HashMap<>(stringProcessing(request));
+        String name = clientParams.get("name");
+        int age = Integer.parseInt(clientParams.get("age"));
+        double salary = Double.parseDouble(clientParams.get("salary").replace(",", "."));
+        int id = getNewId();
+        return saveClient(id, new Client(id, name, age, salary));
+    }
+
+    public static String deleteClient(String id){
+        File dir = new File(usersPath+"/"+id+".bin");
+        if(dir.exists()){
+            if(dir.delete()){
+                return Main.answerOK+"client is deleted\n";
+            }
+        }
+        return Main.answer404+"Client is not exist";
+    }
+
+    public static String getClient(String id){
+        File dir = new File(usersPath+"/"+id+".bin");
+        if(dir.exists()){
+                StringBuilder sb = new StringBuilder();
+                sb.append(Main.answerOK);
+                try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream(dir))){
+                    Client client = (Client)ois.readObject();
+                    JSONFormatterImpl json = new JSONFormatterImpl();
+                    sb.append(json.marshall(client)).append("<br/>");
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+                return sb.toString();
+        }
+        return Main.answer404+"Client is not exist";
+    }
+
+    public static String getClientsList(){
+        File dir = new File(usersPath);
+        StringBuilder sb = new StringBuilder();
+        sb.append(Main.answerOK);
+        for(File f : dir.listFiles()){
+            try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream(usersPath+"/"+f.getName()))){
+                Client client = (Client)ois.readObject();
+                JSONFormatterImpl json = new JSONFormatterImpl();
+                sb.append(json.marshall(client)).append("<br/>");
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+        return sb.toString();
+    }
+
+    private static int getNewId(){
+        File dir = new File(usersPath);
+        int max = 0;
+        for(File f : dir.listFiles()){
+            int id = Integer.valueOf(f.getName().substring(0, f.getName().indexOf(".")));
+            if(id > max) max = id;
+        }
+        return max+1;
+    }
+
+    private static String saveClient(int id, Client client){
+        StringBuilder answer = new StringBuilder();
+        answer
+                .append(Main.answerOK)
+                .append("id:").append(id).append("\n");
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(usersPath + "/" + id + ".bin"))) {
+            oos.writeObject(client);
+        }catch(Exception e){}
+        return answer.toString();
+    }
+
+    private static Map<String, String> stringProcessing (String request){
+        Map<String, String> paramList = new HashMap<>();
+        String param = request.substring(request.indexOf("?")+1);
+        String [] paramsArr = param.split("&");
+        for (String i : paramsArr){
+            paramList.put(i.substring(0, i.indexOf("=")), i.substring(i.indexOf("=")+1, i.length()));
+        }
+        return paramList;
+    }
+}
---
+++ src/main/java/problem/Main.java
@@ -0,0 +1,84 @@
+package problem;
+
+import Logic.ClientLogic;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Properties;
+
+public class Main {
+    public static final String answerOK = "HTTP/1.0 200 OK\r\n" + "Content-Type: text/html\r\n" + "\r\n";
+    public static final String answer404 = "HTTP/1.0 404 Not found\r\n" + "Content-Type: text/html\r\n" + "\r\n";
+    private static final String propPath = "src/main/java/problem/config/prop";
+    private static final String commandCreate = "/user/create?";
+    private static final String commandDelete = "/user/delete/";
+    private static final String commandList = "/user/list/";
+    private static final String commandGetUser = "/user/";
+
+    public static void main(String[] arg){
+        try {
+            Properties property = new Properties();
+            property.load(new FileInputStream(propPath));
+            int port = Integer.parseInt(property.getProperty("http.port"));
+            ServerSocket serverSocket = new ServerSocket(port);
+            System.out.println("Server is waiting for connection");
+
+            while(true) {
+                Socket client = serverSocket.accept();
+                BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                BufferedWriter out = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+
+                String request = br.readLine();
+                if(request != null) {
+                    System.out.println(request);
+
+                }
+
+                String answer = "";
+                if (request.contains(commandCreate)) {
+                    String [] reqArr = request.split(" ");
+                    for (String iReqArr : reqArr) {
+                        if (iReqArr.startsWith(commandCreate) ) {
+                            answer = ClientLogic.createClient(iReqArr);
+                        }
+                    }
+
+                }else if(request.contains(commandDelete)){
+                    String [] reqArr = request.split(" ");
+                    for (String iReqArr : reqArr) {
+                        if (iReqArr.startsWith(commandDelete) ) {
+                            int start = iReqArr.lastIndexOf('/')+1;
+                            String id = iReqArr.substring(start, iReqArr.length());
+                            answer = ClientLogic.deleteClient(id);
+                        }
+                    }
+
+                }else if(request.contains(commandList)){
+                    answer = ClientLogic.getClientsList();
+
+                }else if(request.contains(commandGetUser)){
+                    String [] reqArr = request.split(" ");
+                    for (String iReqArr : reqArr) {
+                        if (iReqArr.startsWith(commandGetUser) ) {
+                            int start = iReqArr.lastIndexOf('/')+1;
+                            String id = iReqArr.substring(start, iReqArr.length());
+                            answer = ClientLogic.getClient(id);
+                        }
+                    }
+                }
+
+                out.write(answer);
+                out.flush();
+                out.close();
+                br.close();
+
+                if ( request.contains("quit") )
+                    break;
+            }
+            System.out.println("Server is stopped");
+
+        }catch(Exception ignored){
+
+        }
+    }
+}
